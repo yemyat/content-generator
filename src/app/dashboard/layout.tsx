dd7436f7ccac533f194aa862d6d77b4ce759cdf7
Ye Myat Min <yemyat@gmail.com>
2025-02-06 21:38:38 +0800

    chore: move editor

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -30,7 +30,7 @@ export default function DashboardLayout({
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
+        <header className="sticky top-0 z-10 flex h-16 shrink-0 items-center gap-2 bg-white">
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 h-4" />
