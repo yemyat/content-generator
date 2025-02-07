5218becf873e064b3a5e8245b9147820140e651c
Ye Myat Min <yemyat@gmail.com>
2025-02-07 15:26:28 +0800

    chore: update sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -100,7 +100,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             </Button>
           </SidebarMenu>
         </SidebarGroup>
-        <NavMain items={data.navMain} />
+        {/* <NavMain items={data.navMain} /> */}
         <NavProjects projects={data.projects} />
         <NavSecondary items={data.navSecondary} className="mt-auto" />
 
