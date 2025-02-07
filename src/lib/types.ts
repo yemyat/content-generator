035ef7947a65fabe332af14b9a78c29bcea63455
Ye Myat Min <yemyat@gmail.com>
2025-02-07 10:38:56 +0800

    chore: working state

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,4 +1,4 @@
-import { Value } from "@udecode/plate";
+import { type Value } from "@udecode/plate";
 import { type BasicElementsPlugin } from "@udecode/plate-basic-elements/react";
 import { type BasicMarksPlugin } from "@udecode/plate-basic-marks/react";
 import { type IndentListPlugin } from "@udecode/plate-indent-list/react";
@@ -7,6 +7,7 @@ import {
   type ListPlugin,
   type ListItemContentPlugin,
 } from "@udecode/plate-list/react";
+import { type MarkdownPlugin } from "@udecode/plate-markdown";
 import { type TPlateEditor } from "@udecode/plate/react";
 import { type autoformatPlugin } from "~/components/plugins/autoformat-plugin";
 import { type FloatingToolbarPlugin } from "~/components/plugins/floating-toolbar-plugin";
