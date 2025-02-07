dce55086e6d26aa37d3eb075081ce40a0835f21b
Ye Myat Min <yemyat@gmail.com>
2025-02-07 16:54:41 +0800

    chore: theme toolbar

--- src/components/plate-ui/floating-toolbar.tsx
+++ src/components/plate-ui/floating-toolbar.tsx
@@ -71,7 +71,7 @@ export const FloatingToolbar = withRef<
       <Toolbar
         ref={ref}
         className={cn(
-          "scrollbar-hide absolute z-50 overflow-x-auto whitespace-nowrap rounded-md border bg-popover p-1 opacity-100 shadow-md print:hidden",
+          "absolute z-50 overflow-x-auto whitespace-nowrap rounded-full border bg-sidebar p-1 opacity-100 shadow-md scrollbar-hide print:hidden",
           "max-w-[80vw]",
         )}
         {...rootProps}
